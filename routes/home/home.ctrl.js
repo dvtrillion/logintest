75cda85ea6449acb34a1d54945c85de08f154154
dvtrillion <wbtrillion@gmail.com>
2022-12-04 17:41:09 +0900

    깃 허브 저장소

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+
+const main = (req, res) => {
+    res.render("home/index");
+};
+
+
+const login = (req, res) => {
+    res.render("home/login");
+};
+
+module.exports = {
+    main,
+    login,
+};
